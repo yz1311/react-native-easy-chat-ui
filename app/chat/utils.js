63aa96200cbeda7cbf98c26e542bf7d22895e02c
yangzhao <yz1311@foxmail.com>
2021-04-30 22:01:16 +0800

    修复对iphone12 pro max的兼容

--- app/chat/utils.js
+++ app/chat/utils.js
@@ -1,18 +1,18 @@
 import moment from 'moment'
 import {Dimensions, Platform} from 'react-native'
 import { EMOJIS_ZH, invertKeyValues } from '../source/emojis'
-const X_WIDTH = 375
-const X_HEIGHT = 812
-const XSMAX_WIDTH = 414
-const XSMAX_HEIGHT = 896
-const PAD_WIDTH = 768
-const PAD_HEIGHT = 1024
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
+const PAD_WIDTH = 768;
+const PAD_HEIGHT = 1024;
 const IPHONE12_WIDTH = 390;
 const IPHONE12_HEIGHT = 844;
-const IPADPRO11_WIDTH = 834
-const IPADPRO11_HEIGHT = 1194
-const IPADPRO129_HEIGHT = 1024
-const IPADPRO129_WIDTH = 1366
+const IPHONE12MAX_WIDTH = 428;
+const IPHONE12MAX_HEIGHT = 926;
+const IPHONE12MINI_WIDTH = 360;
+const IPHONE12MINI_HEIGHT = 780;
 
 const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window')
 
@@ -149,13 +149,17 @@ const changeEmojiText = (textContent, type = 'zh') => {
 
 const isIPhoneX = () => {
   return (
-    Platform.OS === 'ios' &&
-    ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
-      (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT)) ||
-    ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
-      (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT)) ||
+      Platform.OS === 'ios' &&
+      ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
+          (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT)) ||
+      ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
+          (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT)) ||
       ((D_HEIGHT === IPHONE12_HEIGHT && D_WIDTH === IPHONE12_WIDTH) ||
-      (D_HEIGHT === IPHONE12_WIDTH && D_WIDTH === IPHONE12_HEIGHT))
+          (D_HEIGHT === IPHONE12_WIDTH && D_WIDTH === IPHONE12_HEIGHT)) ||
+      ((D_HEIGHT === IPHONE12MAX_HEIGHT && D_WIDTH === IPHONE12MAX_WIDTH) ||
+          (D_HEIGHT === IPHONE12MAX_WIDTH && D_WIDTH === IPHONE12MAX_HEIGHT)) ||
+      ((D_HEIGHT === IPHONE12MINI_HEIGHT && D_WIDTH === IPHONE12MINI_WIDTH) ||
+          (D_HEIGHT === IPHONE12MINI_WIDTH && D_WIDTH === IPHONE12MINI_HEIGHT))
   );
 }
 export {
